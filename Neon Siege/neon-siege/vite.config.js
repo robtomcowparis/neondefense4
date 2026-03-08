34e30a23a52f6eb614f1568508ac37dfbff982db
robtomcowparis <robtomcowparis@gmail.com>
2026-03-08 15:39:52 -0600

    NeonSiegeUpgradeFix

--- Neon Siege/neon-siege/vite.config.js
+++ Neon Siege/neon-siege/vite.config.js
@@ -5,6 +5,7 @@ import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  base: '/neon-siege/',
   root: 'src',
   publicDir: '../public',
   build: {
